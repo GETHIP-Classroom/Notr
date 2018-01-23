4cf24ad4f46cd8aee61c353a9641392c8af27086
Emma Johnson <emmajohnson@Emmas-MacBook-Air.local>
2018-01-22 20:59:29 -0600

    added nav bar to calendar pages and removed the starts of meteor

--- join.js
+++ join.js
@@ -1,17 +1,1 @@
-Template.register.events({
-  'submit form': function(event){
-      event.preventDefault();
-      var username = $('[name=username]').val();
-      var password = $('[name=password]').val();
-      Accounts.createUser({
-        username: username,
-        password: password
-      }, function(error){
-        if(error){
-            console.log(error.reason); // Output error if registration fails
-        } else {
-            Router.go('home'); // Redirect user if registration succeeds
-        }
-      });
-  }
-});
+
